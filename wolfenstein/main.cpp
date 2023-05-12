45bd7e4bba6de315fee9911b0d077b2dea9597aa
Rhys Fonville <rhys.fonville@gmail.com>
2023-05-11 21:12:39 -0400

    Now works and with shader.

--- wolfenstein/main.cpp
+++ wolfenstein/main.cpp
@@ -5,24 +5,25 @@
 #include <chrono>
 
 #define SCREEN_AREA (screen_width * screen_height)
-#define MAP_AREA (map_width * map_height)
 
 #define WALL '#'
 
-constexpr int screen_width = 120u;
-constexpr int screen_height = 40u;
+const std::string char_gradient = " .\'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+
+constexpr int screen_width = 120;
+constexpr int screen_height = 40;
 
 float playerX = 8.0f;
 float playerY = 8.0f;
 float playerA = 0.0f;
 
-constexpr int map_width = 16u;
-constexpr int map_height = 16u;
+constexpr int map_width = 16;
+constexpr int map_height = 16;
 
 std::string map;
 
-float fov = (float)(std::numbers::pi * 4.0);
-float max_view_depth = MAP_AREA;
+float fov = (float)(std::numbers::pi / 8.0f);
+float max_view_depth = 16.0f;
 
 int main() {
 	std::string screen;
@@ -31,21 +32,21 @@ int main() {
 	SetConsoleActiveScreenBuffer(console);
 	DWORD chars_written = 0ul;
 
-	map += "################";
+	map += "     ###########";
+	map += "     #         #";
+	map += "     #         #";
+	map += "     #         #";
+	map += "     #         #";
+	map += "######         #";
 	map += "#              #";
+	map += "#         ##   #";
+	map += "#         ##   #";
 	map += "#              #";
+	map += "######         #";
 	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
-	map += "#              #";
+	map += "#         #    #";
+	map += "#         #    #";
+	map += "#         #    #";
 	map += "################";
 
 	auto tp1 = std::chrono::system_clock::now();
@@ -98,17 +99,25 @@ int main() {
 			int ceiling = (float)(screen_height / 2.0f) - screen_height / distance_to_wall;
 			int floor = screen_height - ceiling;
 
+			char shade_char = char_gradient[0];
+			for (int i = char_gradient.size()-1; i >= 1; i--) {
+				if (distance_to_wall < max_view_depth / (float)i) {
+					shade_char = char_gradient[i];
+					break;
+				}
+			}
+
 			for (int y = 0; y < screen_height; y++) {
 				if (y <= ceiling)
 					screen[y * screen_width + x] = ' ';
 				else if (y > ceiling && y <= floor)
-					screen[y * screen_width + x] = '#';
+					screen[y * screen_width + x] = shade_char;
 				else
-					screen[y * screen_width + x] = ' ';
+					screen[y * screen_width + x] = '.';
 			}
 		}
 
 		screen[screen_width * screen_height - 1] = '\0';
-		WriteConsoleOutputCharacterA(console, &screen[0], screen_width * screen_height, { 0, 0 }, &chars_written);
+		WriteConsoleOutputCharacterA(console, screen.c_str(), screen_width * screen_height, { 0, 0 }, &chars_written);
 	}
 }
